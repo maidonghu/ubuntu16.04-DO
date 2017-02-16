ee60e68f02a833f7811db3560c1a341da7cf54f3
maidonghu <maidonghu@users.noreply.github.com>
2017-02-15 16:13:24 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,11 @@ echo -e '$pswd\n$pswd' | passwd
 apt update && apt upgrade -y
 timedatectl set-timezone America/Vancouver
 apt install ntp
-
+sed -i 's/ports 22/ports 50009/' /etc/ssd/sshd_config
+systemctl restart sshd
+ufw allow 50009/tcp
+ufw allow 6800/tcp
+ufw enable
+ufw logging on
+ufw logging medium
+echo 'Please logoff and login again with SSH!' 
