050f9e03aec76ee0831c0bf2f6a64bc8ce45c203
maidonghu <maidonghu@users.noreply.github.com>
2017-02-15 19:20:38 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 read -s -p "Enter Root Password: "  pswd
-echo -e '$pswd\n$pswd' | passwd
+echo -e "$pswd\n$pswd" | passwd
 # passwd
 apt update && apt upgrade -y
 timedatectl set-timezone America/Vancouver
