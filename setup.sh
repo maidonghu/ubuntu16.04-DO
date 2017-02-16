edce67341a5b9c6526c5a2ef611a23bba099368c
maidonghu <maidonghu@users.noreply.github.com>
2017-02-15 18:57:42 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,4 +13,8 @@ ufw allow 6800/tcp
 ufw enable
 ufw logging on
 ufw logging medium
+mkfs.ext4 -F /dev/disk/by-id/scsi-0DO_Volume_volume-nyc1-01
+mkdir -p /mnt/volume-nyc1-01
+mount -o discard,defaults /dev/disk/by-id/scsi-0DO_Volume_volume-nyc1-01 /mnt/volume-nyc1-01
+echo /dev/disk/by-id/scsi-0DO_Volume_volume-nyc1-01 /mnt/volume-nyc1-01 ext4 defaults,nofail,discard 0 0 | sudo tee -a /etc/fstab
 echo 'Please logoff and login again with SSH!' 
