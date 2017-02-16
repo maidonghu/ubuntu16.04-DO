73f34b72e3724a6570c31bf3f21ddf64ffa379b2
maidonghu <maidonghu@users.noreply.github.com>
2017-02-16 03:40:29 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ ufw logging medium
 mkfs.ext4 -F /dev/disk/by-id/scsi-0DO_Volume_volume-nyc1-01
 mkdir -p /mnt/volume-nyc1-01
 mount -o discard,defaults /dev/disk/by-id/scsi-0DO_Volume_volume-nyc1-01 /mnt/volume-nyc1-01
+echo /dev/disk/by-id/scsi-0DO_Volume_volume-nyc1-01 /mnt/volume-nyc1-01 ext4 defaults,nofail,discard 0 0 | sudo tee -a /etc/fstab
 wget https://raw.githubusercontent.com/maidonghu/ubuntu16.04-DO/master/makeswap.sh
 chmod +x makeswap.sh
 ./makeswap.sh
-echo /dev/disk/by-id/scsi-0DO_Volume_volume-nyc1-01 /mnt/volume-nyc1-01 ext4 defaults,nofail,discard 0 0 | sudo tee -a /etc/fstab
 echo 'Please logoff and login again with SSH!' 
