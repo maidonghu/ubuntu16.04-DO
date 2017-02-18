609d4784dd33959f1a49344356712c599102fb35
maidonghu <maidonghu@users.noreply.github.com>
2017-02-18 12:14:23 -0800

    Update ss-libev.sh

--- ss-libev.sh
+++ ss-libev.sh
@@ -1,7 +1,8 @@
-wget https://github.com/shadowsocks/shadowsocks-libev/archive/master.zip
-unzip master.zip
-cd shadowsocks-libev-master
-./scripts/deb4xenial.sh
+#wget https://github.com/shadowsocks/shadowsocks-libev/archive/master.zip
+#unzip master.zip
+#cd shadowsocks-libev-master
+#./scripts/deb4xenial.sh
+# apt install libsodium-dev
 wget https://raw.githubusercontent.com/maidonghu/ubuntu16.04-DO/master/shadowsocks.conf
 mv shadowsocks.conf /etc/shadowsocks-libev/config.json
 ufw allow 53214/tcp
