b13b7194be874597a2da466c7f9ea268df73b7ca
maidonghu <maidonghu@users.noreply.github.com>
2017-02-18 11:53:10 -0800

    Update ss-libev.sh

--- ss-libev.sh
+++ ss-libev.sh
@@ -1,11 +1,7 @@
-git clone https://github.com/shadowsocks/shadowsocks-libev.git
-cd shadowsocks-libev
-git submodule update --init --recursive
-cd ..
-apt install --no-install-recommends apg gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libudns-dev automake libmbedtls-dev
-./autogen.sh && dpkg-buildpackage -b -us -uc
-cd ..
-dpkg -i shadowsocks-libev*.deb
+wget https://github.com/shadowsocks/shadowsocks-libev/archive/master.zip
+unzip master.zip
+cd shadowsocks-libev-master
+./scripts/deb4xenial.sh
 wget https://raw.githubusercontent.com/maidonghu/ubuntu16.04-DO/master/shadowsocks.conf
 mv shadowsocks.conf /etc/shadowsocks-libev/config.json
 ufw allow 53214/tcp
