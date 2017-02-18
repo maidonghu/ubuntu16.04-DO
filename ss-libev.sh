bfd063cd7b43d293b75422a2648bb1c3840f1a9e
maidonghu <maidonghu@users.noreply.github.com>
2017-02-18 11:18:40 -0800

    Update ss-libev.sh

--- ss-libev.sh
+++ ss-libev.sh
@@ -6,3 +6,8 @@ apt install --no-install-recommends apg gettext build-essential autoconf libtool
 ./autogen.sh && dpkg-buildpackage -b -us -uc
 cd ..
 dpkg -i shadowsocks-libev*.deb
+wget https://raw.githubusercontent.com/maidonghu/ubuntu16.04-DO/master/shadowsocks.conf
+mv shadowsocks.conf /etc/shadowsocks-libev/config.json
+ufw allow 53214/tcp
+systemctl start shadowsocks-libev
+systemctl restart shadowsocks-libev
