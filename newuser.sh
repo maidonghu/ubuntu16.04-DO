1720642798a8bc23f1672b5ae629d5a0dcbfb9c4
maidonghu <maidonghu@users.noreply.github.com>
2017-02-21 17:59:25 -0800

    Update newuser.sh

--- newuser.sh
+++ newuser.sh
@@ -2,7 +2,6 @@ su - mike
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 touch ./.ssh/authorized_keys
-exit
 sudo cat ./.ssh/authorized_keys | tee -a /home/mike/.ssh/authorized_keys
 su - mike
 chmod 600 ~/.ssh/authorized_keys
