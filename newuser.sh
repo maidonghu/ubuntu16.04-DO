36fb60d32c3b1dbf833acf276935232f243e7f18
maidonghu <maidonghu@users.noreply.github.com>
2017-02-21 17:45:28 -0800

    Create newuser.sh

---
+++ newuser.sh
@@ -0,0 +1,11 @@
+su - mike
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+touch ./.ssh/authorized_keys
+exit
+cat ./.ssh/authorized_keys | tee -a /home/mike/.ssh/authorized_keys
+su - mike
+chmod 600 ~/.ssh/authorized_keys
+echo export GOPATH=$HOME/gocode | sudo tee -a ./.profile
+echo PATH=$PATH:$HOME/gocode/bin | sudo tee -a ./.profile
+exit
