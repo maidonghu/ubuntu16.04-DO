2b81cd51b5956d7fbb58c4eaa06839f4fd2c5bf2
maidonghu <maidonghu@users.noreply.github.com>
2017-02-21 18:02:15 -0800

    Update newuser.sh

--- newuser.sh
+++ newuser.sh
@@ -4,7 +4,6 @@ mkdir ~/.ssh
 chmod 700 ~/.ssh
 touch ./.ssh/authorized_keys
 sudo cat ./.ssh/authorized_keys | tee -a /home/mike/.ssh/authorized_keys
-su - mike
 chmod 600 ~/.ssh/authorized_keys
 echo export GOPATH=$HOME/gocode | sudo tee -a ./.profile
 echo PATH=$PATH:$HOME/gocode/bin | sudo tee -a ./.profile
