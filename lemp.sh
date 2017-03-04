1b590efc6bc8db459c87247012d0d4d64455f573
maidonghu <maidonghu@users.noreply.github.com>
2017-03-04 04:44:54 -0800

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -2,9 +2,8 @@ sudo apt-get update
 sudo apt-get install nginx -y
 sudo ufw allow 'Nginx HTTP'
 
-read -s -p "Enter MySQL Root Password: "  pswd
-echo "mysql-server mysql-server/$pswd password root" | sudo debconf-set-selections
-echo "mysql-server mysql-server/$pswd password root" | sudo debconf-set-selections
+#read -s -p "Enter MySQL Root Password: "  pswd
+#echo "mysql-server mysql-server/$pswd password root" | sudo debconf-set-selections
+#echo "mysql-server mysql-server/$pswd password root" | sudo debconf-set-selections
 sudo -E apt-get install -y -q mysql-server
-
-sudo mysql_secure_installation
+sudo mysql -sfu root < "mysql_secure_installation.sql"
