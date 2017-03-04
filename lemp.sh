bffe3077bb8997cea18072c32fb94d57607df56d
maidonghu <maidonghu@users.noreply.github.com>
2017-03-04 04:51:15 -0800

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -9,3 +9,5 @@ sudo -E apt-get install -y -q mysql-server
 sudo mysql -sfu root < "mysql_secure_installation.sql"
 
 sudo apt-get install php-fpm php-mysql
+sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
+sudo systemctl restart php7.0-fpm
